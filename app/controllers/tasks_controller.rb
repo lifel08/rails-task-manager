e96046af3eb11205a47c1616042226742d806817
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-03-02 17:44:19 +0100

    tasks on rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,53 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+    # console
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
+
+
+  # create_table "tasks", force: :cascade do |t|
+  #   t.string "title"
+  #   t.text "details"
+  #   t.boolean "completed", default: false
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # main page
+  get 'tasks', to: "tasks#index"
+  # display form
+  get "tasks/new", to: "tasks#new"
+  # detail page, as :rename the prefix
+  get "tasks/:id", to: "tasks#show", as: :task
+  # send form
+  post "tasks", to: "tasks#create"
+  #edit a task, longer urls don't gert displayed as prefixes
+  get "tasks/:id/edit", to: "tasks#edit", as: :tasks_edit
+  # patch / paste the edit on the old version
+  patch "tasks/:id", to: "tasks#update"
+  # delete task
+  delete "tasks/:id", to: "tasks#destroy", as: :tasks_delete
 end
